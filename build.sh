bb7aa4acdc6e5c8625e2ac2e3aa85008186733ac
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-13 20:13:50 +0200

    Add scripts

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git clone https://github.com/PeterKementzey/graph-library-for-lean4 build/deps/Graph
+
+(cd build/deps/Graph; git checkout --detach e73f70c848de80cce8cbff50aa8586496bc47c1d; leanpkg build lib)
+
+leanpkg build bin LINK_OPTS=build/deps/Graph/build/lib/libGraph.a
---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd build
+
+rm *.olean
+rm -rf bin temp Test
