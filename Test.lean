c602efd2ef1f1daff58fd3f0179d13fa3bd83c17
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-16 14:15:49 +0200

    Add comment about importing

--- Test.lean
+++ Test.lean
@@ -1,4 +1,4 @@
-import Graph
+import Graph.Graph -- This will only import the Graph structure and basic functionality, you can use `import Graph` to import all default algorithms
 
 def g : Graph Nat Nat := (Graph.empty.addVertex 0).1
 
