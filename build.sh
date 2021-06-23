b4fb0cb5146642d7f1d37d9c9c3bf451889675e4
Peter Kementzey <45865060+PeterKementzey@users.noreply.github.com>
2021-06-23 23:47:38 +0200

    Update commit hash and lean version

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/PeterKementzey/graph-library-for-lean4 build/deps/Graph
 
-(cd build/deps/Graph; git checkout --detach d6f6287a0c2c126582864f16c74b60f833a2033f; leanpkg build lib)
+(cd build/deps/Graph; git checkout --detach d7d5e2d698a142789e479edc9a90e0b3598dcf7a; leanpkg build lib)
 
 leanpkg build bin LINK_OPTS=build/deps/Graph/build/lib/libGraph.a
